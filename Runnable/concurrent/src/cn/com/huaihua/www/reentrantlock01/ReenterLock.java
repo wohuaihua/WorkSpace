e9ac0c165aebf4381e7e0fc49e307b5e9e3b88ea
wohuaihua <31920287+wohuaihua@users.noreply.github.com>
2017-11-20 23:20:23 +0800

    20171120

---
+++ Runnable/concurrent/src/cn/com/huaihua/www/reentrantlock01/ReenterLock.java
@@ -0,0 +1,34 @@
+package cn.com.huaihua.www.reentrantlock01;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReenterLock implements Runnable{
+	
+	public static ReentrantLock lock=new ReentrantLock();
+	public static int i=0;
+	@Override
+	public void run() {
+		for(int j=0;j<1000000;j++) {
+			lock.lock();//多次获得锁
+			lock.lock();
+			try {
+				i++;
+			} finally {
+				lock.unlock();//必须多次释放锁
+				lock.unlock();
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		ReenterLock tl=new ReenterLock();
+		Thread t1=new Thread(tl);
+		Thread t2=new Thread(tl);
+		t1.start();
+		t2.start();
+		t1.join();
+		t2.join();
+		System.out.println(i);
+	}
+			
+}
